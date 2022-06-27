eb600905301aaa6bccdbae4b58046826b2caea82
DouglasRodr <wandersondouglasr@gmail.com>
2022-06-26 22:27:04 -0300

    Aula 12.24 - Refatorando transição de status de pedidos

--- src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/StatusPedido.java
@@ -1,17 +1,29 @@
 package com.algaworks.algafood.domain.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import java.util.Arrays;
+import java.util.List;
 
-@Getter
-@AllArgsConstructor
 public enum StatusPedido {
 
 	CRIADO("Criado"),
-	CONFIRMADO("Confirmado"),
-	ENTREGUE("Entregue"),
-	CANCELADO("Cancelado");
+	CONFIRMADO("Confirmado", CRIADO),
+	ENTREGUE("Entregue", CONFIRMADO),
+	CANCELADO("Cancelado", CRIADO);
 	
 	private String descricao;
+	private List<StatusPedido> statusAnteriores;
+	
+	StatusPedido(String descricao, StatusPedido... statusAnteriores) {
+		this.descricao = descricao;
+		this.statusAnteriores = Arrays.asList(statusAnteriores);
+	}
+	
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public boolean naoPodeAlterarPara(StatusPedido novoStatus) {
+		return !novoStatus.statusAnteriores.contains(this);
+	}
 	
 }
