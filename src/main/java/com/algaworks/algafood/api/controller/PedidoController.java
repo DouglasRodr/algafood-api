ad7824efe22e050df35db2677ab4510ba347d7cb
DouglasRodr <wandersondouglasr@gmail.com>
2022-06-26 21:28:06 -0300

    Aula 12.20 - Otimizando a query de pedidos e retornando model resumido

--- src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/PedidoController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.PedidoModelAssembler;
+import com.algaworks.algafood.api.assembler.PedidoResumoModelAssembler;
 import com.algaworks.algafood.api.model.PedidoModel;
+import com.algaworks.algafood.api.model.PedidoResumoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.repository.PedidoRepository;
 import com.algaworks.algafood.domain.service.EmissaoPedidoService;
@@ -24,21 +26,24 @@ public class PedidoController {
     @Autowired
     private EmissaoPedidoService emissaoPedido;
     
+	@Autowired
+	private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+    
     @Autowired
     private PedidoModelAssembler pedidoModelAssembler;
     
-    @GetMapping
-    public List<PedidoModel> listar() {
-        List<Pedido> todosPedidos = pedidoRepository.findAll();
-        
-        return pedidoModelAssembler.toCollectionModel(todosPedidos);
-    }
-    
-    @GetMapping("/{pedidoId}")
-    public PedidoModel buscar(@PathVariable Long pedidoId) {
-        Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
-        
-        return pedidoModelAssembler.toModel(pedido);
-    }            
+	@GetMapping
+	public List<PedidoResumoModel> listar() {
+		List<Pedido> todosPedidos = pedidoRepository.findAll();
+		
+		return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+	}
+	
+	@GetMapping("/{pedidoId}")
+	public PedidoModel buscar(@PathVariable Long pedidoId) {
+		Pedido pedido = emissaoPedido.buscarOuFalhar(pedidoId);
+		
+		return pedidoModelAssembler.toModel(pedido);
+	}   
 
 }
