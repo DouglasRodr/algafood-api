a074755ef37f2e1322af2de0ddb8bf375db62008
DouglasRodr <wandersondouglasr@gmail.com>
2022-07-03 12:22:41 -0300

    Aula 19.15 - Adicionando hypermedia em recursos com paginação

--- src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.api.model;
 
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
 import com.algaworks.algafood.api.model.view.RestauranteView;
 import com.fasterxml.jackson.annotation.JsonView;
 
@@ -7,14 +10,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+@Relation(collectionRelation = "cozinhas")
 @Setter
 @Getter
-public class CozinhaModel {
+public class CozinhaModel extends RepresentationModel<CozinhaModel> {
 
 	@ApiModelProperty(example = "1")
 	@JsonView(RestauranteView.Resumo.class)
 	private Long id;
-
+	
 	@ApiModelProperty(example = "Brasileira")
 	@JsonView(RestauranteView.Resumo.class)
 	private String nome;
