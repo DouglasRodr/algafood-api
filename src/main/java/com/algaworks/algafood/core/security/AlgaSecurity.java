2c2fd22b14e462e2fa074d1ccd8ca0c78d8b0402
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-22 10:57:08 -0300

    Aula 23.38 - Corrigindo restrição de acessos Client Credentials Flow

--- src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
+++ src/main/java/com/algaworks/algafood/core/security/AlgaSecurity.java
@@ -11,33 +11,37 @@ import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @Component
 public class AlgaSecurity {
-	
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	@Autowired
 	private PedidoRepository pedidoRepository;
 
 	public Authentication getAuthentication() {
 		return SecurityContextHolder.getContext().getAuthentication();
 	}
-	
+
 	public Long getUsuarioId() {
 		Jwt jwt = (Jwt) getAuthentication().getPrincipal();
-		
+
 		return jwt.getClaim("usuario_id");
 	}
-	
+
 	public boolean gerenciaRestaurante(Long restauranteId) {
-	    if (restauranteId == null) {
-	        return false;
-	    }
-	    
-	    return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
-	}  
-	
-	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
-	    return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
+		if (restauranteId == null) {
+			return false;
+		}
+
+		return restauranteRepository.existsResponsavel(restauranteId, getUsuarioId());
 	}
-	
+
+	public boolean gerenciaRestauranteDoPedido(String codigoPedido) {
+		return pedidoRepository.isPedidoGerenciadoPor(codigoPedido, getUsuarioId());
+	}
+
+	public boolean usuarioAutenticadoIgual(Long usuarioId) {
+		return getUsuarioId() != null && usuarioId != null && getUsuarioId().equals(usuarioId);
+	}
+
 }
