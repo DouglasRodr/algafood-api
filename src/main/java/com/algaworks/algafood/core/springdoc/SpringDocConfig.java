2d056469d404f934b3728d31e987d45bfdab9f0a
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-27 21:29:33 -0300

    Aula 26.4 - Configurando múltiplas documentações em um só projeto

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,31 +1,48 @@
 package com.algaworks.algafood.core.springdoc;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 
 @Configuration
 public class SpringDocConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("AlgaFood API")
-                        .version("v1")
-                        .description("REST API do AlgaFood")
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://springdoc.com")
-                        )
-                ).externalDocs(new ExternalDocumentation()
-                        .description("AlgaWorks")
-                        .url("https://algaworks.com")
-                );
-    }
+	@Bean
+	public GroupedOpenApi groupedOpenApiv1() {
+		return GroupedOpenApi.builder()
+				.group("AlgaFood API v1")
+				.packagesToScan("com.algaworks.algafood.api")
+				.pathsToMatch("/v1/**")
+				.addOpenApiCustomiser(openApi -> {
+					openApi.info(new Info().title("AlgaFood API v1")
+							.version("v1")
+							.description("REST API do AlgaFood")
+							.license(new License().name("Apache 2.0").url("http://springdoc.com")))
+							.externalDocs(new ExternalDocumentation()
+									.description("AlgaWorks")
+									.url("https://algaworks.com"));
+				}).build();
+	}
+
+	@Bean
+	public GroupedOpenApi groupedOpenApiv2() {
+		return GroupedOpenApi.builder()
+				.group("AlgaFood API v2")
+				.packagesToScan("com.algaworks.algafood.api")
+				.pathsToMatch("/v2/**")
+				.addOpenApiCustomiser(openApi -> {
+					openApi.info(new Info().title("AlgaFood API v2")
+							.version("v2")
+							.description("REST API do AlgaFood")
+							.license(new License().name("Apache 2.0").url("http://springdoc.com")))
+							.externalDocs(new ExternalDocumentation()
+									.description("AlgaWorks")
+									.url("https://algaworks.com"));
+				}).build();
+	}
 
 }
