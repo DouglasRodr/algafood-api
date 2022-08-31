b6599359775f9cf2476681564f5b7a96c3d4fb7e
DouglasRodr <wandersondouglasr@gmail.com>
2022-08-31 19:54:59 -0300

    Aula 26.7 - Descrevendo as operações de endpoints na documentação

--- src/main/java/com/algaworks/algafood/api/v2/openapi/controller/CidadeControllerV2OpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v2/openapi/controller/CidadeControllerV2OpenApi.java
@@ -5,6 +5,7 @@ import org.springframework.hateoas.CollectionModel;
 import com.algaworks.algafood.api.v2.model.CidadeModelV2;
 import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,14 +13,20 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Cidades")
 public interface CidadeControllerV2OpenApi {
 
+	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeModelV2> listar();
 
+	@Operation(summary = "Busca uma cidade por Id")
 	CidadeModelV2 buscar(Long cidadeId);
 
+	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, "
+			+ "necessita de um estado e um nome válido")
 	CidadeModelV2 adicionar(CidadeInputV2 cidadeInput);
 
+	@Operation(summary = "Atualizado uma cidade por ID")
 	CidadeModelV2 atualizar(Long cidadeId, CidadeInputV2 cidadeInput);
-
+	
+	@Operation(summary = "Excluir uma cidade por ID")
 	void remover(Long cidadeId);
 
 }
