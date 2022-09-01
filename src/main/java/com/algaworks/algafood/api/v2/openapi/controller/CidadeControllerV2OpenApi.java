be833debb3a8e52e218837edc6d36124f9c43986
DouglasRodr <wandersondouglasr@gmail.com>
2022-09-01 10:28:06 -0300

    Aula 26.14 - Descrevendo o modelo de representação de problema

--- src/main/java/com/algaworks/algafood/api/v2/openapi/controller/CidadeControllerV2OpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v2/openapi/controller/CidadeControllerV2OpenApi.java
@@ -7,7 +7,10 @@ import com.algaworks.algafood.api.v2.model.input.CidadeInputV2;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -18,7 +21,12 @@ public interface CidadeControllerV2OpenApi {
 	@Operation(summary = "Lista as cidades")
 	CollectionModel<CidadeModelV2> listar();
 
-	@Operation(summary = "Busca uma cidade por Id")
+	@Operation(summary = "Busca uma cidade por Id", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cidade inválido",
+					content = @Content(schema = @Schema(ref = "Problema"))
+			)
+	})
 	CidadeModelV2 buscar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 	@Operation(summary = "Cadastra uma cidade", description = "Cadastro de uma cidade, "
--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -1,21 +1,28 @@
 package com.algaworks.algafood.core.springdoc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+
+import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
 import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
@@ -47,7 +54,10 @@ public class SpringDocConfig {
 							.externalDocs(new ExternalDocumentation()
 									.description("AlgaWorks")
 									.url("https://algaworks.com"))
-							.tags(tags());
+							.tags(tags())
+							.components(new Components().schemas(
+			                        gerarSchemas()
+			                ));
 					addGlobalResponses(openApi);
 				}).build();
 	}
@@ -66,8 +76,10 @@ public class SpringDocConfig {
 							.externalDocs(new ExternalDocumentation()
 									.description("AlgaWorks")
 									.url("https://algaworks.com"))
-							.tags(tags());
-					
+							.tags(tags())
+							.components(new Components().schemas(
+			                        gerarSchemas()
+			                ));					
 					addGlobalResponses(openApi);				
 				}).build();
 	}
@@ -109,5 +121,17 @@ public class SpringDocConfig {
 	private List<Tag> tags() {
 		return Arrays.asList(new Tag().name("Cidades").description("Gerencia as cidades"));
 	}
+	
+    private Map<String, Schema> gerarSchemas() {
+        final Map<String, Schema> schemaMap = new HashMap<>();
+
+        Map<String, Schema> problemSchema = ModelConverters.getInstance().read(Problem.class);
+        Map<String, Schema> problemObjectSchema = ModelConverters.getInstance().read(Problem.Object.class);
+
+        schemaMap.putAll(problemSchema);
+        schemaMap.putAll(problemObjectSchema);
+
+        return schemaMap;
+    }
 	
 }
