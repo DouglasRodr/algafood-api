218573d566ee140c550e03de999c11edce9a6819
DouglasRodr <wandersondouglasr@gmail.com>
2022-09-01 09:56:38 -0300

    Aula 26.11 - Descrevendo códigos de status de respostas de forma global

--- src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -13,8 +13,11 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
 
 @Configuration
@@ -45,6 +48,7 @@ public class SpringDocConfig {
 									.description("AlgaWorks")
 									.url("https://algaworks.com"))
 							.tags(tags());
+					addGlobalResponses(openApi);
 				}).build();
 	}
 
@@ -63,11 +67,33 @@ public class SpringDocConfig {
 									.description("AlgaWorks")
 									.url("https://algaworks.com"))
 							.tags(tags());
+					
+					addGlobalResponses(openApi);				
 				}).build();
+	}
+	
+	private void addGlobalResponses(OpenAPI openApi) {
+		openApi.getPaths()
+        .values()
+        .stream()
+        .flatMap(pathItem -> pathItem.readOperations().stream())
+        .forEach(operation -> {
+            ApiResponses responses = operation.getResponses();
+
+            ApiResponse apiResponseNaoEncontrado = new ApiResponse().description("Recurso não encontrado");
+            ApiResponse apiResponseErroInterno = new ApiResponse().description("Erro interno no servidor");
+            ApiResponse apiResponseSemRepresentacao = new ApiResponse()
+                    .description("Recurso não possui uma representação que poderia ser aceita pelo consumidor");
+
+            responses.addApiResponse("404", apiResponseNaoEncontrado);
+            responses.addApiResponse("406", apiResponseSemRepresentacao);
+            responses.addApiResponse("500", apiResponseErroInterno);
+        });
+	
 	}
 	
 	private List<Tag> tags() {
 		return Arrays.asList(new Tag().name("Cidades").description("Gerencia as cidades"));
 	}
-
+	
 }
